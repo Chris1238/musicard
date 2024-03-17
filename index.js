85c6950046288a6c2a1d1cc9bfcc6af3ef17a7d2
Chris1238 <113909888+Chris1238@users.noreply.github.com>
2024-03-17 16:33:40 +0000

    Upload files

--- index.js
+++ index.js
@@ -12,16 +12,9 @@ function registerFont(fontPath, fontName) {
 }
 
 module.exports = { registerFont }
-
 registerFont("extrabold.ttf", "extrabold");
-registerFont("regular.ttf", "regular");
 registerFont("semibold.ttf", "semibold");
 
 // import themes
 const { Classic } = require("./themes/classic.js");
-const { ClassicPro } = require("./themes/classicpro.js");
-const { Dynamic } = require("./themes/dynamic.js");
-const { Mini } = require("./themes/mini.js");
-const { MiniPro } = require("./themes/minipro.js");
-
-module.exports = { Classic, ClassicPro, Dynamic, Mini, MiniPro };
+module.exports = { Classic };
--- themes/classic.js
+++ themes/classic.js
@@ -1,131 +1,151 @@
-const { createCanvas, loadImage } = require("@napi-rs/canvas");
-const { cropImage } = require("cropify");
+const { createCanvas, loadImage } = require("@napi-rs/canvas")
+const { colorFetch } = require("../utils/colorFetch")
 
 async function Classic({
-    thumbnailImage,
-    backgroundColor,
+    thumbnail,
     progress,
-    progressColor,
-    progressBarColor,
     name,
-    nameColor,
     author,
-    authorColor,
     startTime,
     endTime,
-    timeColor
 }) {
-    if (!progress) progress = 10;
+    if (!progress) progress = 0
     if (!name) name = "Musicard"
     if (!author) author = "By Unburn"
     if (!startTime) startTime = "0:00"
-    if (!endTime) endTime = "0:00"
+    if (!endTime) endTime = "4:00"
 
-    if (!progressBarColor) progressBarColor = "#5F2D00";
-    if (!progressColor) progressColor = "#FF7A00";
-    if (!backgroundColor) backgroundColor = "#070707"
-    if (!nameColor) nameColor = "#FF7A00"
-    if (!authorColor) authorColor = "#696969"
-    if (!timeColor) timeColor = "#FF7A00"
+    const color = `#${await colorFetch("auto", 50, thumbnail)}`
+    let thumbnailImage
 
-    const noImageSvg = `<svg width="837" height="837" viewBox="0 0 837 837" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="837" height="837" fill="${progressColor}"/>
-    <path d="M419.324 635.912C406.035 635.912 394.658 631.18 385.195 621.717C375.732 612.254 371 600.878 371 587.589C371 574.3 375.732 562.923 385.195 553.46C394.658 543.997 406.035 539.265 419.324 539.265C432.613 539.265 443.989 543.997 453.452 553.46C462.915 562.923 467.647 574.3 467.647 587.589C467.647 600.878 462.915 612.254 453.452 621.717C443.989 631.18 432.613 635.912 419.324 635.912ZM371 490.941V201H467.647V490.941H371Z" fill="${backgroundColor}"/>
+    try {
+        thumbnailImage = await loadImage(thumbnail, {
+            requestOptions: {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+                }
+            }
+        })
+    } catch {
+        throw new Error("Failed to load thumnail image")
+    }
+
+    if (progress < 2) {
+        progress = 2
+    } else if (progress > 98) {
+        progress = 98
+    }
+
+    if (name.length > 15) {
+        name = name.slice(0, 15) + ".."
+    }
+
+    if (author.length > 15) {
+        author = author.slice(0, 15) + ".."
+    }
+
+    /* if (name.length > 15) name = name.slice(0, 13) + ".."
+    if (author.length > 15) author = author.slice(0, 13) + ".." */
+
+    try {
+        const canvas = createCanvas(1280, 450)
+        const ctx = canvas.getContext("2d")
+
+        const backgroundSvg = `<svg width="1280" height="450" viewBox="0 0 1280 450" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="816" height="270" rx="60" fill="#0e0e0e"/>
+    <rect y="300" width="816" height="150" rx="60" fill="#0e0e0e"/>
     </svg>`
 
-    const noimageDataUrl = `data:image/svg+xml;base64,${Buffer.from(noImageSvg).toString('base64')}`;
+        const backgroundDataUrl = `data:image/svg+xml;base64,${Buffer.from(backgroundSvg).toString("base64")}`
+        const background = await loadImage(backgroundDataUrl)
+        ctx.drawImage(background, 0, 0)
 
-    if (!thumbnailImage) {
-        thumbnailImage = noimageDataUrl
-    };
+        const thumbnailCanvas = createCanvas(564, 564)
+        const thumbnailCtx = thumbnailCanvas.getContext("2d")
 
-    let thumbnail;
+        const thumbnailSize = Math.min(thumbnailImage.width, thumbnailImage.height)
+        const thumbnailX = (thumbnailImage.width - thumbnailSize) / 2
+        const thumbnailY = (thumbnailImage.height - thumbnailSize) / 2
+        const thumbnailCornerRadius = 45
 
-    try {
-        thumbnail = await loadImage(await cropImage({
-            imagePath: thumbnailImage,
-            borderRadius: 20,
-            width: 837,
-            height: 837,
-            cropCenter: true
-        }))
-    } catch {
-        thumbnail = await loadImage(await cropImage({
-            imagePath: noimageDataUrl,
-            borderRadius: 20,
-            width: 837,
-            height: 837,
-            cropCenter: true
-        }))
-    }
+        thumbnailCtx.beginPath()
+        thumbnailCtx.moveTo(0 + thumbnailCornerRadius, 0)
+        thumbnailCtx.arcTo(thumbnailCanvas.width, 0, thumbnailCanvas.width, thumbnailCanvas.height, thumbnailCornerRadius)
+        thumbnailCtx.arcTo(thumbnailCanvas.width, thumbnailCanvas.height, 0, thumbnailCanvas.height, thumbnailCornerRadius)
+        thumbnailCtx.arcTo(0, thumbnailCanvas.height, 0, 0, thumbnailCornerRadius)
+        thumbnailCtx.arcTo(0, 0, thumbnailCanvas.width, 0, thumbnailCornerRadius)
+        thumbnailCtx.closePath()
+        thumbnailCtx.clip()
 
-    if (progress < 10) {
-        progress = 10
-    } else if (progress > 100) {
-        progress = 100
-    }
+        thumbnailCtx.drawImage(thumbnailImage, thumbnailX, thumbnailY, thumbnailSize, thumbnailSize, 0, 0, thumbnailCanvas.width, thumbnailCanvas.height)
+        ctx.drawImage(thumbnailCanvas, 837, 8, 435, 435)
 
-    if (name.length > 18) {
-        name = name.slice(0, 18) + "...";
-    }
+        const completed = (progress / 100) * 670
+        const circleX = completed + 60
 
-    if (author.length > 18) {
-        author = author.slice(0, 18) + "...";
-    }
+        const progressBarCanvas = createCanvas(670, 25)
+        const progressBarCtx = progressBarCanvas.getContext("2d")
+        const cornerRadius = 10
 
-    try {
-        const canvas = createCanvas(2458, 837);
-        const ctx = canvas.getContext("2d");
+        progressBarCtx.beginPath()
+        progressBarCtx.moveTo(cornerRadius, 0)
+        progressBarCtx.lineTo(670 - cornerRadius, 0)
+        progressBarCtx.arc(670 - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI)
+        progressBarCtx.lineTo(670, 25 - cornerRadius)
+        progressBarCtx.arc(670 - cornerRadius, 25 - cornerRadius, cornerRadius, 0, 0.5 * Math.PI)
+        progressBarCtx.lineTo(cornerRadius, 25)
+        progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI)
+        progressBarCtx.lineTo(0, cornerRadius)
+        progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI)
+        progressBarCtx.closePath()
+        progressBarCtx.fillStyle = "#ababab"
+        progressBarCtx.fill()
+        progressBarCtx.beginPath()
+        progressBarCtx.moveTo(cornerRadius, 0)
+        progressBarCtx.lineTo(completed - cornerRadius, 0)
+        progressBarCtx.arc(completed - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI)
+        progressBarCtx.lineTo(completed, 25)
+        progressBarCtx.lineTo(cornerRadius, 25)
+        progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI)
+        progressBarCtx.lineTo(0, cornerRadius)
+        progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI)
+        progressBarCtx.closePath()
+        progressBarCtx.fillStyle = color
+        progressBarCtx.fill()
 
-        // ---------------------------------
-        const backgroundSvg = `<svg width="2458" height="837" viewBox="0 0 2458 837" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="1568" height="512" rx="20" fill="${backgroundColor}"/>
-    <rect y="565" width="1568" height="272" rx="20" fill="${backgroundColor}"/>
-    </svg>`;
+        const circleCanvas = createCanvas(1000, 1000)
+        const circleCtx = circleCanvas.getContext("2d")
 
-        const backgroundDataUrl = `data:image/svg+xml;base64,${Buffer.from(backgroundSvg).toString('base64')}`;
+        const circleRadius = 20
+        const circleY = 97
 
-        const background = await loadImage(backgroundDataUrl);
+        circleCtx.beginPath()
+        circleCtx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI)
+        circleCtx.fillStyle = color
+        circleCtx.fill()
 
-        ctx.drawImage(background, 0, 0);
-        // ---------------------------------
-        ctx.drawImage(thumbnail, 1621, 0);
-        // ---------------------------------
-        const completed = 1342 * progress / 100;
+        ctx.drawImage(progressBarCanvas, 70, 340, 670, 25)
+        ctx.drawImage(circleCanvas, 10, 255, 1000, 1000)
 
-        const progressBarSvg = `<svg width="1342" height="76" viewBox="0 0 1342 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect y="13" width="1342" height="47" rx="30" fill="${progressBarColor}"/>
-    <rect y="13" width="${completed}" height="47" rx="30" fill="${progressColor}"/>
-    <rect x="${completed - 40}" y="3" width="69.4422" height="69.4422" rx="34.7211" fill="${progressColor}" stroke="${backgroundColor}" stroke-width="6"/>
-    </svg>`;
+        ctx.fillStyle = color
+        ctx.font = "75px extrabold"
+        ctx.fillText(name, 70, 120)
 
-        const progressDataUrl = `data:image/svg+xml;base64,${Buffer.from(progressBarSvg).toString('base64')}`;
+        ctx.fillStyle = "#b8b8b8"
+        ctx.font = "50px semibold"
+        ctx.fillText(author, 75, 190)
 
-        const progressBar = await loadImage(progressDataUrl);
+        ctx.fillStyle = "#fff"
+        ctx.font = "30px semibold"
+        ctx.fillText(startTime, 70, 410)
 
-        ctx.drawImage(progressBar, 113, 635);
-
-        // ---------------------------------
-        ctx.fillStyle = `${nameColor}`;
-        ctx.font = "124px extrabold";
-        ctx.fillText(name, 113, 230);
-
-        ctx.fillStyle = `${authorColor}`;
-        ctx.font = "87px semibold";
-        ctx.fillText(author, 113, 370);
-
-        ctx.fillStyle = `${timeColor}`;
-        ctx.font = "50px semibold";
-        ctx.fillText(startTime, 113, 768);
-
-        ctx.fillStyle = `${timeColor}`;
-        ctx.font = "50px semibold";
-        ctx.fillText(endTime, 1332, 768);
-        // ---------------------------------
-        return canvas.toBuffer("image/png");
+        ctx.fillStyle = "#fff"
+        ctx.font = "30px semibold"
+        ctx.fillText(endTime, 670, 410)
+        return canvas.toBuffer("image/png")
     } catch (e) {
-        throw new Error(e.message);
+        throw new Error(e.message)
     }
 }
 
-module.exports = { Classic };
+module.exports = { Classic }
